491f95f865985995d2c60e7ce22e55d55c53ffea
subaru <subaru519kqka@gmail.com>
2023-12-11 04:26:24 +0900

    [add] add template codes for pytorch.

---
+++ src/model.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class BaseModel(nn.Module):
+    def __init__(self):
+        super(BaseModel, self).__init__()
+        # TODO: ここにコードを書く
+
+    def forward(self, x):
+        # TODO: ここにコードを書く
+        return x
---
+++ src/utils.py
@@ -0,0 +1,11 @@
+def is_env_notebook():
+    """Determine wheather is the environment Jupyter Notebook"""
+    if 'get_ipython' not in globals():
+        # Python shell
+        return False
+    env_name = get_ipython().__class__.__name__
+    if env_name == 'TerminalInteractiveShell':
+        # IPython shell
+        return False
+    # Jupyter Notebook
+    return True
